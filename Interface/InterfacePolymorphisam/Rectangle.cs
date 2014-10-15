34e5812752c42c858aa79f82286b669f44567ab4
KishorNaik <kishor.naik011.net@gmail.com>
2014-10-15 22:27:59 +0530

    Polymorphism using Interface

---
+++ Interface/InterfacePolymorphisam/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.InterfacePolymorphisam
+{
+    public class Rectangle: IShape
+    {
+        public void Draw()
+        {
+            System.Console.WriteLine("Rectangle  - Draw Method");
+        }
+    }
+}
